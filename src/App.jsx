cb160f4786e63498e0a6302bbf66229cdaf01b28
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2026-06-14 11:05:46 +0600

    ui implementation and basic setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import TaskBoard from "./components/tasks/TaskBoard";
 
 export default function App(){
     return (
         <div className="min-h-screen flex flex-col lg:flex-row">
           <Sidebar />
           <main class="flex-1 flex flex-col min-h-0">
-            
+            <Header />
+            <TaskBoard />
           </main>
         </div>
     );
